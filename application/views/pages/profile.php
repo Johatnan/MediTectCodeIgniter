7474ba88e1ff457b2807179cf79f8b4ad4bd532c
Johatnan <84165800+Johatnan@users.noreply.github.com>
2021-06-17 21:33:06 +0800

    Done with profile backend

--- application/views/pages/profile.php
+++ application/views/pages/profile.php
@@ -4,7 +4,7 @@
             </p>
             <!-- <pre>
               <?php 
-                print_r($meditect_client);
+                print_r($meditect_client[0]);
                 // print_r(count($meditect_client)-1);
               ?> -->
         </div>
@@ -210,16 +210,16 @@
                         if(!empty($meditect_client) and !empty($meditect_client[0])){
                           if (!empty($meditect_client[0]['user_first_name']) and $meditect_client[0]['user_first_name'] != '') {
                             echo'
-                              <input type="text" class="form-control" name = "user_first_name" value="'.$meditect_client[0]['user_first_name'].'">
+                              <input type="text" class="form-control" name = "user_first_name" value="'.$meditect_client[0]['user_first_name'].'" required>
                             ';        
                           } else {
                             echo'
-                              <input type="text" class="form-control" name = "user_first_name">
+                              <input type="text" class="form-control" name = "user_first_name" required>
                             ';  
                           }
                         } else {
                           echo'
-                            <input type="text" class="form-control" name = "user_first_name">
+                            <input type="text" class="form-control" name = "user_first_name" required>
                           ';  
                         }                     
                       ?>
@@ -232,16 +232,16 @@
                         if(!empty($meditect_client) and !empty($meditect_client[0])){
                           if (!empty($meditect_client[0]['user_last_name']) and $meditect_client[0]['user_last_name'] != '') {
                             echo'
-                              <input type="text" class="form-control" name = "user_last_name" value="'.$meditect_client[0]['user_last_name'].'">
+                              <input type="text" class="form-control" name = "user_last_name" value="'.$meditect_client[0]['user_last_name'].'" required>
                             ';  
                           } else {
                             echo'
-                              <input type="text" class="form-control" name = "user_last_name">
+                              <input type="text" class="form-control" name = "user_last_name" required>
                             ';  
                           }                       
                         } else {
                           echo'
-                            <input type="text" class="form-control" name = "user_last_name">
+                            <input type="text" class="form-control" name = "user_last_name" required>
                           ';  
                         }                     
                       ?>
@@ -396,7 +396,7 @@
                   </div>
                   <div class="col-12">
                     <label for="inputPassword4" class="form-label">Profile Picture</label>
-                    <input class="form-control p-1" name="profile_picture" type="file" id="formFile">
+                    <input class="form-control p-1" name="profile_picture" type="file" id="formFile" value="John">
                   </div>
                   <div class="col-6 mt-3 signInBtn">
                       <button type="button" class="btn btn-primary signInBtn" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit Profile</button>
@@ -1064,14 +1064,14 @@
                                 ';
                               } else if ($meditect_client[$i]['rating'] - $ctr >= 0.5) {
                                 echo '
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
+                                    <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z"/>
                                   </svg>
                                 ';
                               } else {
                                 echo '
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                                   </svg>
                                 ';
                               }
