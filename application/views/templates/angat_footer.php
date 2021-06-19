04725a9a63321bdffad614f7913d1c8ea5c3617b
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2021-06-19 22:22:02 +0800

    Clear DB integration on sign up

--- application/views/templates/angat_footer.php
+++ application/views/templates/angat_footer.php
@@ -20,10 +20,10 @@ if(isset($_POST['signup'])){
            //1. Setup Database connection
 
             $epassword = md5($password);
-            $servername = "localhost";
-            $db_username = "root"; //xampp default
-            $db_password = "";  //xampp default
-            $database = "meditect_database";
+            $servername = "eu-cdbr-west-01.cleardb.com";
+            $db_username = "bb372c9ecfbe6e"; //xampp default
+            $db_password = "f8312940";  //xampp default
+            $database = "heroku_5b8ce60be14add9";
 
             $conn = mysqli_connect($servername, $db_username, $db_password, $database);
 
